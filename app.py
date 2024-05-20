7e3628d15d73ad1d9a741fa3bafbc95f9b937637
MrAndreiL <lunguandrei759@gmail.com>
2024-05-20 22:31:46 +0300

    update with support for web pubsub

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 from azure.cosmos import CosmosClient
 import uuid
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
+from azure.cosmos import CosmosClient
+from azure.messaging.webpubsubservice import WebPubSubServiceClient
 
 app = Flask(__name__)
 
@@ -17,10 +21,22 @@ database = client.get_database_client(DATABASE_NAME)
 
 container = database.get_container_client(CONTAINER_NAME)
 
+part1 = "Endpoint=sb://emailspam.servicebus.windows.net/;SharedAccess"
+part2 = "KeyName=RootManageSharedAccessKey;SharedAccessKey=mvfrWyMCwI+kAN"
+part3 = "MMKe9eQhaC/47foWkC4+ASbMInOqE="
+
+NAMESPACE_CONNECTION_STRING = part1 + part2 + part3
+QUEUE_NAME = "bprocessing"
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+async def send_message(sender, message):
+    single_message = ServiceBusMessage(str(message))
+    await sender.send_messages(single_message)
 
 
 @app.route('/check_spam', methods=['POST'])
@@ -30,6 +46,9 @@ def check_spam():
     data["id"] = str(uuid.uuid4())
     data["email"] = email_text
     container.create_item(data)
+    client = ServiceBusClient.from_connection_string(NAMESPACE_CONNECTION_STRING)
+    sender = client.get_queue_sender(QUEUE_NAME)
+    send_message(sender, data)
     # Dummy spam check logic
     if "spam" in email_text.lower():
         result = "This is spam."
