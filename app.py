0c00109e1e52d2839c70f1a5cd58d26f8c91768a
MrAndreiL <lunguandrei759@gmail.com>
2024-05-20 15:18:19 +0300

    add cosmos connection

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request
+from flask import json
+import uuid
+import asyncio
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
+from azure.cosmos import CosmosClient
+from azure.messaging.webpubsubservice import WebPubSubServiceClient
 
 app = Flask(__name__)
+
+DATABASE_NAME = "azurecosmosemailserver"
+CONTAINER_NAME = "Items"
+
+part1 = "AccountEndpoint=https://azurecosmosemails"
+part2 = "erver.documents.azure.com:443/;AccountKey=RI8PZOR8rROhmucmF"
+part3 = "k0N4ABgpE7tjqBcFIKfPpGKPxeD3b76wYK6uRjBKZl6fhm56xxrZHhXU3euACDbNi5oDg==;"
+
+client = CosmosClient.from_connection_string(part1 + part2 + part3)
+
+database = client.get_database_client(DATABASE_NAME)
+
+container = database.get_container_client(CONTAINER_NAME)
 
 
 @app.route('/')
@@ -11,6 +31,7 @@ def index():
 @app.route('/check_spam', methods=['POST'])
 def check_spam():
     email_text = request.form['email_text']
+    container.create_item(email_text)
     # Dummy spam check logic
     if "spam" in email_text.lower():
         result = "This is spam."
